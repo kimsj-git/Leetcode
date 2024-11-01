ba46e6a4266a970adde28144b9d780bada9c4f26
Suhjeong Kim <kimsj.py@gmail.com>
2024-11-01 22:39:27 +0900

    Time: 46 ms (26.83%), Space: 42.5 MB (61.66%) - LeetHub

--- 2215-find-the-difference-of-two-arrays/2215-find-the-difference-of-two-arrays.kt
+++ 2215-find-the-difference-of-two-arrays/2215-find-the-difference-of-two-arrays.kt
@@ -1,7 +1,5 @@
 class Solution {
     fun findDifference(nums1: IntArray, nums2: IntArray): List<List<Int>> {
-        val onlyNums1 = nums1.toSet() - nums2.toSet()
-        val onlyNums2 = nums2.toSet() - nums1.toSet()
-        return listOf(onlyNums1.toList(), onlyNums2.toList())
+        return listOf((nums1.toSet() - nums2.toSet()).toList(), (nums2.toSet() - nums1.toSet()).toList())
     }
 }
