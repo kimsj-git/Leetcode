d292d7ef78aeac4bd0b45624d7c883f3bac1406e
Suhjeong Kim <kimsj.py@gmail.com>
2024-11-02 20:28:36 +0900

    Time: 2 ms (62.39%), Space: 34.3 MB (34.14%) - LeetHub

---
+++ 0392-is-subsequence/0392-is-subsequence.kt
@@ -0,0 +1,14 @@
+class Solution {
+    fun isSubsequence(s: String, t: String): Boolean {
+        var sIndex = 0
+        var tIndex = 0
+        while (sIndex < s.length && tIndex < t.length) {
+            if (s[sIndex] == t[tIndex]) {
+                sIndex++
+            }
+            tIndex++
+        }
+        if (sIndex < s.length) return false
+        return true
+    }
+}
